81cd98a05a6b67dd7ca2798caac0190ae3ae6ed5
Aditya Hebbar <Adityarhebbar@gmail.com>
2023-11-14 08:01:59 -0500

    added an if for cheaters

--- NumberGuesser.py
+++ NumberGuesser.py
@@ -4,8 +4,8 @@ import math
 def start():
   game()
 def game():
-  lower = int(input("Enter Lower bound:- "))
-  upper = int(input("Enter Upper bound:- "))
+  lower = int(input("Enter Maximum Possible Number:- "))
+  upper = int(input("Enter Smallest Possible Number:- "))
   x = random.randint(lower, upper)
   print("\n\tYou've only ", round(math.log(upper - lower + 1, 2)),
       " chances to guess the integer!\n")
@@ -14,16 +14,19 @@ def game():
       count += 1
       guess = int(input("Guess a number:- "))
       if x == guess:
-          print("Congratulations you did it in ", count, " try")
+          if count == 1:
+              print("You probably cheated so screw you")
+          else:
+            print("Congratulations, it took you ", count, " tries")
           break
       elif x > guess:
           print("You guessed too small!")
       elif x < guess:
           print("You Guessed too high!")
   if count >= math.log(upper - lower + 1, 2):
-      print("\nThe number is %d" % x)
-      print("\tBetter Luck Next time!")
-      x = input(r"\n\would you like to go again? Y\N: ") 
+      print("\nThe number was %d" % x)
+      print("\tSorry mate but you lost!")
+      x = input(r"\n\would you like to play again? Y\N: ") 
       if x.lower() ==  "y":
         start()
       if x.lower() ==  "n":
